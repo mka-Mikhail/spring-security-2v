6d40e84a2ce4e40056590882372a59d7b48b9674
mka-Mikhail <mixailrojnow@yandex.ru>
2023-03-07 21:38:41 +0300

    указали для ролей их ограничения
    есть метод для перевода наших ограничений в SimpleGrantedAuthority

--- src/main/java/com/mka/springsecurity2v/model/Role.java
+++ src/main/java/com/mka/springsecurity2v/model/Role.java
@@ -1,6 +1,29 @@
 package com.mka.springsecurity2v.model;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public enum Role {
-    USER,
-    ADMIN
+    USER(Set.of(Permission.DEVELOPERS_READ)),
+    ADMIN(Set.of(Permission.DEVELOPERS_READ, Permission.DEVELOPERS_WRITE));
+
+    private final Set<Permission> permissions;
+
+    Role(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    //для вывода, какие ограничения имеет роль
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
+
+    //вывод прав, которых имеет роль, при этом мы их мапим к SimpleGrantedAuthority
+    public Set<SimpleGrantedAuthority> getAuthorities() {
+        return getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+    }
 }
