2a3206af214470ce815aa4d4620c0dbdf8559d39
mka-Mikhail <mixailrojnow@yandex.ru>
2023-03-07 20:00:11 +0300

    создан проект

--- src/main/java/com/mka/springsecurity2v/SpringSecurity2vApplication.java
+++ src/main/java/com/mka/springsecurity2v/SpringSecurity2vApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringSecurity2vApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurity2vApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurity2vApplication.class, args);
+    }
 
 }
