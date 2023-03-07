952c37cbae9ca6fb39ac90dcff2b8b8e321fc4b0
mka-Mikhail <mixailrojnow@yandex.ru>
2023-03-07 20:13:05 +0300

    модель Developer

---
+++ src/main/java/com/mka/springsecurity2v/model/Developer.java
@@ -0,0 +1,12 @@
+package com.mka.springsecurity2v.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Developer {
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
