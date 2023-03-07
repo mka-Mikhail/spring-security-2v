39d6e95afee58957b80d50c00d952d94c6c1b87e
mka-Mikhail <mixailrojnow@yandex.ru>
2023-03-07 19:59:58 +0300

    Initial commit

---
+++ src/main/java/com/mka/springsecurity2v/SpringSecurity2vApplication.java
@@ -0,0 +1,13 @@
+package com.mka.springsecurity2v;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity2vApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity2vApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mka/springsecurity2v/SpringSecurity2vApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mka.springsecurity2v;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity2vApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
