84fe3636984778aec0fcb4d532b233c5e9f0d8e3
mka-Mikhail <mixailrojnow@yandex.ru>
2023-03-07 20:13:27 +0300

    контроллер для модели Developer

---
+++ src/main/java/com/mka/springsecurity2v/rest/DeveloperRestController.java
@@ -0,0 +1,30 @@
+package com.mka.springsecurity2v.rest;
+
+import com.mka.springsecurity2v.model.Developer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api/v1/developers")
+public class DeveloperRestController {
+    private List<Developer> developers = Stream.of(
+            new Developer(1L, "Ivan", "Ivanov"),
+            new Developer(2L, "Sergey", "Sergeev"),
+            new Developer(3L, "Petr", "Petrov")
+    ).toList();
+
+    @GetMapping
+    public List<Developer> getAll() {
+        return developers;
+    }
+
+    @GetMapping("/{id}")
+    public Developer getById(@PathVariable Long id) {
+        return developers.stream().filter(developer -> developer.getId().equals(id)).findFirst().orElse(null);
+    }
+}
