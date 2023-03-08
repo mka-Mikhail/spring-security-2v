7f6f66d2bd774ee25d0681ea5bcc05dd18744991
mka-Mikhail <mixailrojnow@yandex.ru>
2023-03-08 16:03:00 +0300

    реализация спрингового юзера и сервиса для поиска его в БД по username

---
+++ src/main/java/com/mka/springsecurity2v/security/SecurityUser.java
@@ -0,0 +1,75 @@
+package com.mka.springsecurity2v.security;
+
+import com.mka.springsecurity2v.model.Status;
+import com.mka.springsecurity2v.model.User;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+//наша реализация спрингового юзера UserDetails, до этого был inMemory юзер от InMemoryUserDetailsManager - типа провайдер
+//этот класс преобразует юзера из БД, которого мы достаём с помощью метода класса UserDetailsServiceImpl, в спринговый юзер UserDetails
+@Data
+public class SecurityUser implements UserDetails {
+    private final String username;
+    private final String password;
+    private final List<SimpleGrantedAuthority> authorities;
+    private final boolean isActive;
+
+    public SecurityUser(String username, String password, List<SimpleGrantedAuthority> authorities, boolean isActive) {
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+        this.isActive = isActive;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
+
+    //юзера из БД мы преобразуем в UserDetails
+    public static UserDetails fromUser(User user) {
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(), user.getPassword(),
+                user.getStatus().equals(Status.ACTIVE),
+                user.getStatus().equals(Status.ACTIVE),
+                user.getStatus().equals(Status.ACTIVE),
+                user.getStatus().equals(Status.ACTIVE),
+                user.getRole().getAuthorities()
+        );
+    }
+}
